cbeb8797c73d40e9c4651d3b50e60da3f412e1e0
cartoonweber <minkbuckstar@gmail.com>
2022-04-01 01:13:41 +1000

    antiBot.sol fix

--- TokenFactory/contracts/AntiBot.sol
+++ TokenFactory/contracts/AntiBot.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -11,12 +12,13 @@ contract AntiBot is Ownable, ReentrancyGuard {
         address router;
         address pair;
         uint256 limitAmount;
-        uint256 limitTime;
-        uint256 disableBlockTime;
+        uint256 amountPerBlock;
         uint256 preTransferTime;
+        uint256 disableBlockTime;
+        uint256 lastTime;
     }
     mapping(address => address) public token_owner;
-    mapping(address => mapping(address => bool)) public whitelists;
+    mapping(address => mapping(address => bool)) public blocklists;
     mapping(address => Config) public configs;
     mapping(address => bool) public isConfigSet;
 
@@ -25,22 +27,22 @@ contract AntiBot is Ownable, ReentrancyGuard {
     function setTokenOwner(address owner) public {
         token_owner[msg.sender] = owner;
     }
-
-    function addWhiteLists(address _token, address[] memory _whitelists)
+    
+    function addBlockLists(address _token, address[] memory _blocklists)
         public
     {
         require(token_owner[_token] == msg.sender, "Not Owner");
-        for (uint256 i = 0; i < _whitelists.length; i++) {
-            whitelists[_token][_whitelists[i]] = true;
+        for (uint256 i = 0; i < _blocklists.length; i++) {
+            blocklists[_token][_blocklists[i]] = true;
         }
     }
 
-    function removeWhiteLists(address _token, address[] memory _whitelists)
+    function removeBlockLists(address _token, address[] memory _blocklists)
         public
     {
         require(token_owner[_token] == msg.sender, "Not Owner");
-        for (uint256 i = 0; i < _whitelists.length; i++) {
-            whitelists[_token][_whitelists[i]] = false;
+        for (uint256 i = 0; i < _blocklists.length; i++) {
+            blocklists[_token][_blocklists[i]] = false;
         }
     }
 
@@ -49,16 +51,18 @@ contract AntiBot is Ownable, ReentrancyGuard {
         address _router,
         address _pair,
         uint256 _limitAmount,
-        uint256 _limitTime,
+        uint256 _amountPerBlock,
+        uint256 _perTransferTime,
         uint256 _disableBlockTime
     ) public {
         require(token_owner[_token] == msg.sender, "Not Owner");
         Config storage _config = configs[_token];
         _config.router = _router;
-        _config.limitTime = _limitTime;
-        _config.limitAmount = _limitAmount;
         _config.pair = _pair;
+        _config.amountPerBlock = _amountPerBlock;
+        _config.limitAmount = _limitAmount;
         _config.disableBlockTime = block.number + _disableBlockTime;
+        _config.preTransferTime = _perTransferTime;
         isConfigSet[_token] = true;
     }
 
@@ -66,19 +70,20 @@ contract AntiBot is Ownable, ReentrancyGuard {
         address from,
         address to,
         uint256 amount
-    ) public view returns (bool) {
+    ) public returns (bool) {
         Config storage _config = configs[msg.sender];
         if (block.number >= _config.disableBlockTime) return true;
         require(
-            whitelists[msg.sender][from] == true &&
-                whitelists[msg.sender][to] == true,
-            "Transfer between not whitelisted users"
+            blocklists[msg.sender][from] != true &&
+                blocklists[msg.sender][to] != true,
+            "Transfer between not blocklisted users"
         );
-        require(amount > 0 && amount <= _config.limitAmount, "Invalid Amount");
+        require(amount > 0 && amount <= _config.limitAmount * (_config.disableBlockTime - block.number), "Invalid Amount");
         require(
-            block.timestamp.sub(_config.preTransferTime) >= _config.limitTime,
+            block.timestamp.sub(_config.preTransferTime * 1000) >= _config.lastTime,
             "Not Transfer Time"
         );
+        _config.lastTime = block.timestamp;
         return true;
     }
 
